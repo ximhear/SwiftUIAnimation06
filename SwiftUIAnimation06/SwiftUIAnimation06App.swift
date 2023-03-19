41c2e25edce5542beacab290194c053c674b1ede
chlee <z000000375@wemakeprice.com>
2023-03-19 11:02:01 +0900

    Initial Commit

---
+++ SwiftUIAnimation06/SwiftUIAnimation06App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimation06App.swift
+//  SwiftUIAnimation06
+//
+//  Created by gzonelee on 2023/03/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimation06App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
